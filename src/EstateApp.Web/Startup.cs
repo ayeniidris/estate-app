2bddfef30749d2848463c9b6c2cc6688f9c84f62
Dris <ayeniidris23@gmail.com>
2020-06-30 21:01:16 +0100

    Added Application Db Context To Startup.cs

--- src/EstateApp.Web/Startup.cs
+++ src/EstateApp.Web/Startup.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EstateApp.Data.DatabaseContexts.ApplicationDbContext;
+using EstateApp.Data.DatabaseContexts.AuthenticationDbContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace EstateApp.Web
 {
@@ -23,6 +21,22 @@ namespace EstateApp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection"),
+
+                sqlServerOptions => {
+                    sqlServerOptions.MigrationsAssembly("EstateApp.Data");
+                }
+                
+            ));
+
+            services.AddDbContextPool<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("ApplicationConnection"),
+                
+                sqlServerOptions => {
+                    sqlServerOptions.MigrationsAssembly("EstateApp.Data");
+                }
+            ));
             
             services.AddControllersWithViews();
         }
